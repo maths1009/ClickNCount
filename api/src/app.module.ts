1ef66433a5a064776fadcf1b4f6c11204acfb225
Tisma <lb.brangeon@gmail.com>
2025-03-20 13:36:37 +0100

    🗑️ Clean code

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClicksModule } from './modules/click/click.module';
@@ -26,7 +24,5 @@ import { ClicksModule } from './modules/click/click.module';
     }),
     ClicksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
